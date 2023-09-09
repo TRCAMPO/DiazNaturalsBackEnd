101d13537d8c3889508b6575a0fea43173a4a36e
jmsb8888 <jose.salamanca03@uptc.edu.co>
2023-09-09 13:35:10 -0500

    Funciones de busqueda y agregación para productos y usuarios

---
+++ DTO/ProductDeleteDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace BACK_END_DIAZNATURALS.DTO
+{
+    public class ProductDeleteDTO
+    {
+        public string name { get; set; }
+        public bool isActive { get; set; }
+    }
+}
---
+++ DTO/ProductSearchDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace BACK_END_DIAZNATURALS.DTO
+{
+    public class ProductSearchDTO
+    {
+        public string search { get; set; } = null!;
+        public string suppliers { get; set; } = null!;
+        public string presentation { get; set; } = null!;
+    }
+}
